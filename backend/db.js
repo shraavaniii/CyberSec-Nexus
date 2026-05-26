8d697ba48f9d2bdcd76be63227f3118cea917a4c
shraavaniii <shravanihendr@email.com>
2026-05-26 23:11:49 +0530

    fixed postgres connection

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,15 @@
 import pkg from "pg"
-
 const { Pool } = pkg
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 })
 
 pool.connect()
   .then(() => console.log("PostgreSQL Connected"))
-  .catch((err) => console.log("Database Error:", err))
+  .catch(err => console.log("Database Error:", err))
 
 export default pool

7168d93649f04a2d486e2582339b28cd38f850f8
shraavaniii <shravanihendr@email.com>
2026-05-26 22:20:56 +0530

    prevent backend crash on db timeout

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,23 @@
 import mysql from "mysql2/promise"
 
-const db = await mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false
-  },
-  connectTimeout: 10000
-})
+let db
 
-console.log("MySQL Connected")
+try {
+  db = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+      rejectUnauthorized: false
+    },
+    connectTimeout: 10000
+  })
+
+  console.log("MySQL Connected")
+} catch (err) {
+  console.log("Database Error:", err)
+}
 
 export default db

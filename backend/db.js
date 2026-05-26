8d22caab6cba2016ddc4ff7ba8fa334cd72e1428
shraavaniii <shravanihendr@email.com>
2026-05-26 21:06:20 +0530

    fixed cloud database connection

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,19 @@
 import mysql from "mysql2"
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Hendre@123",
-  database: "cybersec_nexus"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 
 db.connect((err) => {
   if (err) {
-    console.log("Database Error")
+    console.log("Database Error:", err)
   } else {
     console.log("MySQL Connected")
   }
